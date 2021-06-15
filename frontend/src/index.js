6364767f73c66a48e0b39d02cc951875d0fdcf1d
Fan Yang <fanyang@fans-air.myfiosgateway.com>
2021-06-15 01:22:59 -0400

    reduced unneccessary code from front end

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Community from './components/Community';
 
 ReactDOM.render(
-  <React.StrictMode>
-
-    <Router>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route path="/community" component={Community} />
-      </Switch>
-    </Router>
-
-
-  </React.StrictMode>,
+  <App />
+  ,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
