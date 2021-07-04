b74884f631dfe82000f1efe6cbf302250f9a3b3e
Fan Yang <fanyang@fans-air.myfiosgateway.com>
2021-07-04 01:14:16 -0400

    fixed getCommunityByName with approximate search

--- backend/src/main/java/com/webApp/Utopia/repository/CommunityRepository.java
+++ backend/src/main/java/com/webApp/Utopia/repository/CommunityRepository.java
@@ -2,15 +2,19 @@ package com.webApp.Utopia.repository;
 
 import com.webApp.Utopia.model.Comment;
 import com.webApp.Utopia.model.Community;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CommunityRepository extends MongoRepository<Community, String> {
 
     @Query("{'name':?0}")
     Optional<Community> findByName(String name);
-    Optional<Community> findByNameLike(String name);
+    @Query("{'name': {'$regex': '?0', '$options': 'i'}}")
+    List<Community> findByNameLike(String name);
     void deleteByName(String name);
 }
