b0b05f2f1e54b349eef75f560494ea85e3303a27
Liyuan Lin <lylin@LiyuandeMBP.attlocal.net>
2021-08-14 22:38:03 -0700

    home page menu set up

--- frontend/src/components/NaviBar/NaviBar.js
+++ frontend/src/components/NaviBar/NaviBar.js
@@ -7,10 +7,13 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import SearchIcon from '@material-ui/icons/Search';
-import {Search, StyledInputBase, ButtonBox} from './NaviBarStyles';
+import {Search, StyledInputBase, ButtonBox, SectionDesktop, SearchIconWrapper, SectionMobile} from './NaviBarStyles';
 import { useHistory } from 'react-router-dom';
 import IconButton from '@material-ui/core/IconButton';
 import { useUserContext } from '../Context/UserContext';
+import { Avatar, Menu } from '@material-ui/core';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import MenuItem from '@material-ui/core/MenuItem';
 
 function HomeIcon(props) {
     return (
@@ -24,73 +27,61 @@ function NaviBar(props) {
     const history = useHistory()
     const { username, isLoggedIn, setLogin, setLoginUsername } = useUserContext()
     const [searchContent, setSearchContent] = React.useState();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
     const handleLogOut = (event) => {
         event.preventDefault()
         localStorage.removeItem('token')
         localStorage.removeItem('username')
         setLogin(false)
+        setAnchorEl(null)
     }
  
     const handleSearch = (event) => {
         event.preventDefault()
         history.push(`/discovery/${searchContent}`)
     }
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleToProfile = () => {
+        history.push('/profile')
+    }
     
+    const handleViewCommunities = () => {
+        history.push('/community')
+    }
+
+    const handleCreateCommunity = () => {
+        history.push('/createCommunity')
+    }
+
+    const handleMobileMenuOpen = (event) => {
+        setMobileMoreAnchorEl(event.currentTarget);
+    }
+
+    const handleMobileClose = () => {
+        setMobileMoreAnchorEl(null);
+    }
+
     React.useEffect(() => {
         if(localStorage.getItem("token")){
             setLogin(true)
             const name =  localStorage.getItem('username')? localStorage.getItem('username'): 'user';
             setLoginUsername(name)
-            // const headers = {
-            //     'Content-Type': 'application/json',
-            //     'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem("token")).jwtToken,
-            // }
-            // axios.defaults.headers.common['Authorization'] = `Bearer ` + JSON.parse(localStorage.getItem("token")).jwtToken;
-            // console.log(axios.defaults.headers.common['Authorization'])
-            // axios.get(GET_USER_API +  localStorage.getItem('username'))
-            // .then(
-            //     response => {
-            //         console.log("got")
-            //     }
-            // ).catch(error => {
-            //     console.log(error)
-            // })
-            
         }
-        console.log(isLoggedIn, username)
-        // axios.interceptors.request.use(function (config) {
-        //     if (localStorage.getItem("token")) {
-        //       config.headers.common['Authorization'] = `Bearer ` + JSON.parse(localStorage.getItem("token")).jwtToken;
-        //       console.log("here")
-        //     }
-        //     return config
-        //   }, function (error) {
-        //     history.push('/login')
-        //     return Promise.reject(error)
-        //   })
-
-        //   axios.interceptors.response.use(function (response) {
-        //     return response
-        //   }, function (error) {
-        //     // 对响应错误做点什么
-        //     if (error.response) {
-        //       switch (error.response.status) {
-        //         case 401:
-        //         //   store.commit('del_token')
-        //           history.push('/login')
-        //         case 403:
-        //         //   store.commit('del_token')
-        //            console.log("error")
-        //     }
-        //     }
-        //     return Promise.reject(error)
-        //   })
-          
     }, [])
 
     return (
-        <div>
+        <div style={{
+            flexGrow:1,}}>
             <CssBaseline />
             <AppBar position="relative">
                 <Toolbar>
@@ -98,8 +89,10 @@ function NaviBar(props) {
                 <Typography variant="h6" color="inherit">
                     Utopia
                 </Typography>
-                
                 <Search>
+                    <SearchIconWrapper>
+                    <SearchIcon />
+                    </SearchIconWrapper>
                     <form onSubmit={handleSearch}>
                     <StyledInputBase
                     placeholder="Search…"
@@ -108,23 +101,40 @@ function NaviBar(props) {
                         setSearchContent(event.target.value)
                     }}
                     />
-                    <IconButton type="submit" aria-label="search">
-                    <SearchIcon />
-                    </IconButton>
                     </form>
                 </Search>
-                <ButtonBox>
+                {/* <ButtonBox> */}
+                <div style={{
+                    flexGrow:1,
+                    }}>
+                <SectionDesktop>
                 {isLoggedIn? (
-                    <div style={{flexDirection: 'row', display:'flex'}}>
-                        <Typography color='inherit' align='center' sx={{mr: 3}} nowrap>
+                    <div style={{
+                        flexDirection: 'row', 
+                        display:'flex',
+                        alignItems:'center',
+                        }}>
+                        <Typography color='inherit'>
                             Welcome, {username}
                         </Typography>
-                        <ButtonGroup variant="contained" aria-label="outlined primary button group">
-                        <Button style={{textTransform: 'none'}}
-                        onClick={handleLogOut}>
-                        Log Out
+                        <Button aria-controls="homepage-menu" aria-haspopup="true" onClick={handleClick}>
+                            <Avatar src="/broken-image.jpg" 
+                                style={{
+                                        height: '28px',
+                                        width: '28px'
+                                }}/>
                         </Button>
-                </ButtonGroup>
+                        <Menu
+                         id="homepage-menu"
+                         anchorEl={anchorEl}
+                         keepMounted
+                         open={Boolean(anchorEl)}
+                         onClose={handleClose}>
+                            <MenuItem onClick={handleToProfile}>Profile</MenuItem>
+                            <MenuItem onClick={handleViewCommunities}>My Communities</MenuItem>
+                            <MenuItem onClick={handleCreateCommunity}>Create a community</MenuItem>
+                            <MenuItem onClick={handleLogOut}>Logout</MenuItem>
+                        </Menu>
                     </div>
                     
                 ): (
@@ -145,7 +155,66 @@ function NaviBar(props) {
                     </ButtonGroup>
                     
                 )}
-                </ButtonBox>
+                </SectionDesktop>
+                <SectionMobile sx={{float:'right'}}>
+                    {isLoggedIn? (
+                        <div>
+                        <IconButton
+                        // aria-label="show more"
+                        aria-controls='mobilePage-menu-login'
+                        aria-haspopup="true"
+                        onClick={handleMobileMenuOpen}
+                        color="inherit"
+                        >
+                    <MoreIcon />
+                    </IconButton>
+                    
+                    <Menu
+                        id="mobilePage-menu-login"
+                        anchorEl={mobileMoreAnchorEl}
+                        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+                        keepMounted
+                        open={Boolean(mobileMoreAnchorEl)}
+                        onClose={handleMobileClose}>
+                            <MenuItem onClick={handleToProfile}>Profile</MenuItem>
+                            <MenuItem onClick={handleViewCommunities}>My Communities</MenuItem>
+                            <MenuItem onClick={handleCreateCommunity}>Create a community</MenuItem>
+                            <MenuItem onClick={handleLogOut}>Logout</MenuItem>
+                    </Menu>
+                    </div>
+                    ):(
+                        <div>
+                        <IconButton
+                        // aria-label="show more"
+                        aria-controls='mobilePage-menu'
+                        aria-haspopup="true"
+                        onClick={handleMobileMenuOpen}
+                        color="inherit"
+                        >
+                    <MoreIcon />
+                    </IconButton>
+                    
+                    <Menu
+                        id="mobilePage-menu"
+                        anchorEl={mobileMoreAnchorEl}
+                        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+                        keepMounted
+                        open={Boolean(mobileMoreAnchorEl)}
+                        onClose={handleMobileClose}>
+                            <MenuItem onClick={() => {
+                                history.push('/login')
+                            }}>Login</MenuItem>
+                            <MenuItem onClick={() => {
+                                history.push('/register')
+                            }}>Sign Up</MenuItem>
+                    </Menu>
+                    </div>
+                    )}
+                </SectionMobile>
+                </div>
+                {/* </ButtonBox> */}
                 </Toolbar>
             </AppBar>
         </div>
